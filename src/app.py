9415892cbed6750284c6487d31116ae387eee176
olivia <oliviateoliva@gmail.com>
2024-12-04 15:49:14 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,9 +1,35 @@
 # -*- coding: utf-8 -*-
 
 from fastapi import FastAPI
+from unicorns import storage
+from unicorns import Unicorn
+from unicorns import Location
 
 app = FastAPI()
 
+@app.get("/setup")
+async def setup():
+    return storage.setup()
+
+
 @app.get("/")
-async def hello():
-    return {"hej": "då"}
+async def get_all_unicorns():
+    return storage.fetch_unicorns()
+
+@app.get("/{id}")
+async def get_singular_unicorn(id):
+    return storage.fetch_unicorn(id)
+
+@app.post("/")
+async def post_unicorn(unicorn : Unicorn):
+    return storage.add_unicorn(unicorn)
+
+@app.put("/{id}")
+async def change_unicorn(unicorn : Unicorn):
+    return storage.update_unicorn(unicorn)
+
+@app.delete("/{id}")
+async def delete_unicorn():
+    return storage.delete_unicorn()
+
+
